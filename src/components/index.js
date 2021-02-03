e8bc044d1403d1f62ae594ac956abb3363fabc72
Yash Prakash Mishra <34412462+xcalibre1@users.noreply.github.com>
2021-02-04 01:24:17 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -7,7 +7,69 @@
 //}
 
 function updateStructure(rec1,rec2){
-	//write your code
-}
+	var arr=["top","bottom","left","right","height","width"]
+	for(let key of arr){
+	   if(!rec1.hasOwnProperty(key))
+		   rec1.key="0px"
+           if(!rec2.hasOwnProperty(key))
+		   rec2.key="0px"
+	}
+	let top2=Number(rec2.top.substr(0,rec2.top.length-2)),top1=Number(rec1.top.substr(0,rec1.top.length-2));
+	let left2=Number(rec2.left.substr(0,rec2.left.length-2)),left1=Number(rec1.left.substr(0,rec1.left.length-2));
+	let height2=Number(rec2.height.substr(0,rec2.height.length-2)),height1=Number(rec1.height.substr(0,rec1.height.length-2));
+	let width2=Number(rec2.width.substr(0,rec2.width.length-2)),width1=Number(rec1.width.substr(0,rec1.width.length-2));
+	let right2=Number(rec2.right.substr(0,rec2.width.length-2)),right1=Number(rec1.right.substr(0,rec2.width.length-2));
+	let bottom2=Number(rec2.bottom.substr(0,rec2.width.length-2)),bottom1=Number(rec2.bottom.substr(0,rec2.width.length-2));
+	if(((top2+bottom2)>=(top1+bottom1) && height1>=height2) &&((left2+right2)>(left1+right1) && width1>width2)){
+		let obj={
+                        top: `${Math.abs(top2-top1)}px`,
+                        left: `${Math.abs(left2-left1)}px`,
+                        width: rec2.width,
+                        height: rec2.height,
+			bottom:`${Math.abs(bottom2-bottom1)}px`,
+			right:`${Math.abs(right2-right1)}px`,
+			children:[]}
+		for(let key in rec2 ){
+			if(rec2[key]=="0px")
+				delete obj[key];
+		}
+		for(let key in rec1 ){
+			if(rec1[key]=="0px")
+				delete rec1[key];
+		}
+		return {
+			...rec1,
+                        children: [obj]
+                }
+	}
+	else if(((top2+bottom2)<(top1+bottom1) && height1<height2) &&((left2+right2)<(left1+right1) && width1<width2)){
+		let obj={
+                        top: `${Math.abs(top2-top1)}px`,
+                        left: `${Math.abs(left2-left1)}px`,
+                        width: rec1.width,
+                        height: rec1.height,
+			bottom:`${Math.abs(bottom2-bottom1)}px`,
+			right:`${Math.abs(right2-right1)}px`,
+			children:[]}
+		for(let key in rec1 ){
+			if(rec1[key]=="0px")
+				delete obj[key];
+		}
+		for(let key in rec2 ){
+			if(rec2[key]=="0px")
+				delete rec2[key];
+		}
+		return {
+			...rec2,
+                        children: [obj]
+                }
+	}
+	else{
+		for(let key in rec1 ){
+			if(rec1[key]=="0px")
+				delete rec1[key];
+		}
+		return return {...rec1};
+	}
 
 module.exports = updateStructure;
