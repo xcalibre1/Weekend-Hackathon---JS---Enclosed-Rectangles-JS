9ff16ed7c55e8b68377101a3a5822b17d058b45b
Yash Prakash Mishra <34412462+xcalibre1@users.noreply.github.com>
2021-02-04 11:23:27 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -10,16 +10,19 @@ function updateStructure(rec1,rec2){
 	var arr=["top","bottom","left","right","height","width"]
 	for(let key of arr){
 	   if(!rec1.hasOwnProperty(key))
-		   rec1.key="0px"
-           if(!rec2.hasOwnProperty(key))
-		   rec2.key="0px"
+		   rec1[key]="0px"
+    if(!rec2.hasOwnProperty(key))
+		   rec2[key]="0px"
 	}
+  console.log(rec1);
+  console.log(rec2);
 	let top2=Number(rec2.top.substr(0,rec2.top.length-2)),top1=Number(rec1.top.substr(0,rec1.top.length-2));
 	let left2=Number(rec2.left.substr(0,rec2.left.length-2)),left1=Number(rec1.left.substr(0,rec1.left.length-2));
 	let height2=Number(rec2.height.substr(0,rec2.height.length-2)),height1=Number(rec1.height.substr(0,rec1.height.length-2));
 	let width2=Number(rec2.width.substr(0,rec2.width.length-2)),width1=Number(rec1.width.substr(0,rec1.width.length-2));
-	let right2=Number(rec2.right.substr(0,rec2.width.length-2)),right1=Number(rec1.right.substr(0,rec2.width.length-2));
-	let bottom2=Number(rec2.bottom.substr(0,rec2.width.length-2)),bottom1=Number(rec2.bottom.substr(0,rec2.width.length-2));
+	let right2=Number(rec2.right.substr(0,rec2.right.length-2)),right1=Number(rec1.right.substr(0,rec1.right.length-2));
+	let bottom2=Number(rec2.bottom.substr(0,rec2.bottom.length-2)),bottom1=Number(rec1.bottom.substr(0,rec1.bottom.length-2));
+  console.log(top1,top2,bottom1,bottom2,right1,right2,left1,left2,height1,height2,width1,width2)
 	if(((top2+bottom2)>=(top1+bottom1) && height1>=height2) &&((left2+right2)>(left1+right1) && width1>width2)){
 		let obj={
                         top: `${Math.abs(top2-top1)}px`,
@@ -37,6 +40,7 @@ function updateStructure(rec1,rec2){
 			if(rec1[key]=="0px")
 				delete rec1[key];
 		}
+    console.log(obj)
 		return {
 			...rec1,
                         children: [obj]
@@ -69,7 +73,9 @@ function updateStructure(rec1,rec2){
 			if(rec1[key]=="0px")
 				delete rec1[key];
 		}
-		return return {...rec1};
+    console.log("elseBlock")
+		return  {...rec1};
 	}
+}
 
 module.exports = updateStructure;
