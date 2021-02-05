8a4eadd92977f14aa3ce973eb8b85f7857d12d6f
Yash Prakash Mishra <34412462+xcalibre1@users.noreply.github.com>
2021-02-06 00:35:56 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -23,7 +23,7 @@ function updateStructure(rec1,rec2){
 	let right2=Number(rec2.right.substr(0,rec2.right.length-2)),right1=Number(rec1.right.substr(0,rec1.right.length-2));
 	let bottom2=Number(rec2.bottom.substr(0,rec2.bottom.length-2)),bottom1=Number(rec1.bottom.substr(0,rec1.bottom.length-2));
   console.log(top1,top2,bottom1,bottom2,right1,right2,left1,left2,height1,height2,width1,width2)
-	if((top2+bottom2+height2)>=(top1+bottom1+height1) &&(left2+right2+width2)>=(left1+right1+width1)){
+	if(top2>=top1 && bottom2>=bottom1 && height1>=height2 &&left2>=left1 && right2>=right1 && width1>=width2){
 		let obj={
                         top: `${Math.abs(top2-top1)}px`,
                         left: `${Math.abs(left2-left1)}px`,
@@ -46,7 +46,7 @@ function updateStructure(rec1,rec2){
                         children: [obj]
                 }
 	}
-	else if(((top2+bottom2)<(top1+bottom1) && height1<height2) &&((left2+right2)<(left1+right1) && width1<width2)){
+	else if(top2<=top1 && bottom2<=bottom1 && height1<=height2 &&left2<=left1 && right2<=right1 && width1<=width2){
 		let obj={
                         top: `${Math.abs(top2-top1)}px`,
                         left: `${Math.abs(left2-left1)}px`,
